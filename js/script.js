4ffcf29d515e8718470664aed3722235dae3ff6f
YuraEremok <yura_eremok@mail.ru>
2019-04-08 12:08:45 +0300

    added task №2

--- js/script.js
+++ js/script.js
@@ -1,29 +1,46 @@
-                   //////////////////// TASK N1  //////////////////////////
+//////////////////// TASK N1  //////////////////////////
 
- let FindLongestString =(sen)=>{
+let FindLongestString = (sen) => {
 
-     var sentence = sen.split(' ');
-     var longestLength = 0;
-     var longestWord;
-     for (var i=0;i<sentence.length;i++) {
-         if (sentence[i].length > longestLength) {
-             longestLength = sentence[i].length;
-             longestWord = sentence[i];
-         }
-     }
-     alert(longestWord)
-     return longestWord;
+    var sentence = sen.split(' ');
+    var longestLength = 0;
+    var longestWord;
+    for (var i = 0; i < sentence.length; i++) {
+        if (sentence[i].length > longestLength) {
+            longestLength = sentence[i].length;
+            longestWord = sentence[i];
+        }
+    }
+    alert(longestWord)
+    return longestWord;
 
- }
-
-
+}
 
 
- var enterWord = prompt("enter a sentence and I will choose the longest word", " how to hack NASA with html ")
+var enterSentence = prompt(" TASK №1. Enter a sentence and I will choose the longest word", " how to hack NASA with html ")
 
- FindLongestString(enterWord);
-
-
-                   //////////////////// TASK N2  //////////////////////////
+FindLongestString(enterSentence);
 
 
+//////////////////// TASK N2  //////////////////////////
+
+let WordReverse = (str) => {
+
+    var arr = str.split("")
+
+    let string = arr.reverse()
+
+    string = arr.join("")
+
+    alert(string);
+
+    return string
+
+
+}
+
+var enterWord = prompt(" TASK №2. Enter your word, and I'll write it reverse", "бамбалейо")
+WordReverse(enterWord);
+
+
+//////////////////// TASK N2  //////////////////////////
